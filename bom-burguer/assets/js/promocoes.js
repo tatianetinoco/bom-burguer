860832ef1e10a956121e26774809dd6035579be8
riverkirasamua <riversonsmorais@hotmail.com>
2023-05-29 10:00:40 -0300

    criado arquivo javascript para pagina de promocao

---
+++ bom-burguer/assets/js/promocoes.js
@@ -0,0 +1,10 @@
+console.log("hello word!");
+let test = document.getElementsByClassName("test");
+console.log(test)
+const arrayCards = []
+
+//criar o elemento
+let document.createElement("'")
+//manipular o elemnto
+
+//Adicionar o elemento
