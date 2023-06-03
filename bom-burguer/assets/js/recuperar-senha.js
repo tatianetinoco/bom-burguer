02f83f3432d60dd24d005771396e081a951e578e
tatiane <tatianeperr@gmail.com>
2023-06-03 20:34:34 -0300

    add validação JS

---
+++ bom-burguer/assets/js/recuperar-senha.js
@@ -0,0 +1,52 @@
+const emailInput = document.getElementById('email_recuperar_login_');
+const emailHelper = document.getElementById('email-helper');
+
+
+
+// ---------- TOGGLE---------- //
+
+function togglePopup(input, label) {
+  input.addEventListener('focus', () => {
+    label.classList.add('required-popup');
+  })
+  
+  input.addEventListener('onchange', () => {
+    label.classList.remove('required-popup');
+  })
+}
+
+togglePopup(emailInput);
+
+
+emailInput.addEventListener('input', (event) => {
+  const valueEmail = event.target.value;
+
+  if (valueEmail.includes('@') && valueEmail.includes('.com')){
+    emailInput.classList.remove('error');
+    emailInput.classList.add('correct');
+    emailHelper.classList.remove('visible');
+    inputCorretos.emailInput = true;
+  } else {
+    emailInput.classList.remove('correct');
+    emailInput.classList.add('error');
+    emailHelper.innerText = 'O email precisa incluir um @ e um .com'
+    emailHelper.classList.add('visible');
+    inputCorretos.emailInput = false;
+  }
+})
+
+
+// ---------- ENVIAR FORMULÁRIO---------- //
+let btnSubmit = document.getElementById('enviar');
+let inputCorretos = {
+  emailInput: false,
+}
+
+btnSubmit.addEventListener('click', (e)=> {
+  if(inputCorretos.emailInput == false){
+    e.preventDefault()
+    alert('Os campos obrigatórios precisam ser preenchidos corretamente')
+  }else{
+    alert('Formulário Enviado Com Sucesso')
+  }
+})
