0ef32aecbd9861a4ab894219bf03a9a447988bc8
vivien fares <129123297+vivifares1@users.noreply.github.com>
2023-06-15 12:23:07 -0300

    js

--- bom-burguer/assets/js/contato.js
+++ bom-burguer/assets/js/contato.js
@@ -1,30 +1,30 @@
 // ---------- FUNÇÕES GERAIS -------------- //
 function togglePopup(input, label) {
     // Mostrar popup de campo obrigatório
-    input.addEventListener("focus", () => {
+    input.addEventListener("click", () => {
     label.classList.add("required-popup");
     });
   
     // Ocultar popup de campo obrigatório
-    input.addEventListener("blur", () => {
-    label.classList.remove("required-popup");
+    nomeInput.addEventListener("blur", () => {
+    nomeLabel.classList.remove("required-popup");
     });
 }
   
 function estilizarInputCorreto(input, helper) {
-    helper.classList.remove("visible");
-    input.classList.remove("error");
-    input.classList.add("correct");
+    nomeHelper.classList.remove("visible");
+    nomeInput.classList.remove("error");
+    nomeInput.classList.add("correct");
 }
   
 function estilizarInputIncorreto(input, helper) {
-    helper.classList.add("visible");
-    input.classList.add("error");
-    input.classList.remove("correct");
+    nomeHelper.classList.add("visible");
+    nomeInput.classList.add("error");
+    nomeInput.classList.remove("correct");
 }
 
 let titulo = document.querySelector("h2")
-titulo.style.color= "#00240F";
+titulo.style.color= "blue";
 titulo.style.fontSize ="2rem"
 
 //Primeira interaçao do usuario"//
@@ -33,39 +33,38 @@ let nome = document.getElementById("nome")
 nome.style.color = "#00240F";
 
 let errorText = document.querySelector(".error-text")
-nome.classList.add("error")
+nomeInput.classList.add("error")
 errorText.classList.add("visible")
 
 // Segunda interação com usuario//
 // usuario digitou um nome de usuario valido//
-
-
 // ---------- VALIDAÇÃO NOME ---------- //
-let nomeInput = document.getElementById("nome");
-let nomeLabel = document.querySelector('label[for="nome"]');
+let nomeInput = document.getElementById("nomeInput");
+let nomeLabel = document.querySelector('label[for="nomeLabel"]');
 let nomeHelper = document.getElementById("nome-helper");
 
 togglePopup(nomeInput, nomeLabel)
 
 // Validar valor do input
 nomeInput.addEventListener("change", (e)=> {
-  let valor = e.target.value
+  let value = e.target.value
 
-  if(valor.length < 3){
+  if(value.length < 3){
     // Adicionar estilos dinâmicos se o valor tiver menos de 3 caracteres
     nomeHelper.innerText = "Seu nome precisa ter 3 ou mais caracteres";
     estilizarInputIncorreto(nomeInput, nomeHelper)
-    inputsCorretos.nome = false
-  } else {
+    nomeInputsCorretos.nome = false
+  } else 
+
     // Adicionar estilos dinâmicos se o valor estiver correto
-    estilizarInputCorreto(nomeInput, nomeHelper);
-    inputsCorretos.nome = true
+    { estilizarInputCorreto(nomeInput, nomeHelper);
+    nomeInputsCorretos.nome = true
   }
 })
 
 // ---------- VALIDAÇÃO EMAIL ---------- //
-let emailInput = document.getElementById("email");
-let emailLabel = document.querySelector('label[for="email"]');
+let emailInput = document.getElementById("emailImput");
+let emailLabel = document.querySelector('label[for="emaillabel"]');
 let emailHelper = document.getElementById("email-helper");
 
 togglePopup(emailInput, emailLabel)
@@ -74,20 +73,19 @@ togglePopup(emailInput, emailLabel)
 emailInput.addEventListener("change", (e)=> {
   let valor = e.target.value
 
-  if(valor.includes("@") && valor.includes(".com")){
+  if(value.includes("@") && value.includes(".com")){
     // Adicionar estilos dinâmicos se o valor estiver correto
     estilizarInputCorreto(emailInput, emailHelper);
     inputsCorretos.email = true
   } else {
-    // Adicionar estilos dinâmicos se o valor tiver menos de 3 caracteres
+    // Adicionar estilos dinâmicos se o valor tiver valor incorreto
     emailHelper.innerText = "Precisa inserir um email válido";
     estilizarInputIncorreto(emailInput, emailHelper);
     inputsCorretos.email = false
   }
-})
 btnContatoEnviar.addEventListener("click", (e)=>{
     if(inputsCorretos.nome == false ||
-      inputsCorretos.email == false ||){
+      inputsCorretos.email == false ||) {
       e.preventDefault()
       alert('Os campos obrigatorios precisam ser preenchidos corretamente')
     }else{
